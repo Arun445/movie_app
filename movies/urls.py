2001b867569db95501191faba6892b64bfaeaa76
arunas445 <skittlessxit@gmail.com>
2021-03-02 13:01:49 +0200

    added a delete function

--- movies/urls.py
+++ movies/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('create/', views.create, name="create"),
     path('update/<str:movie_id>', views.update, name="update"),
+    path('delete/<str:movie_id>', views.delete, name='delete'),
 ]
